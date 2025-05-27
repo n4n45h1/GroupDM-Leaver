b12ce0b6a4b974aaaed3ea01a68d5c446c45489a
Nanachi <a@techz.jp>
2025-05-28 08:55:52 +0900

    Create token-get.js

---
+++ token-get.js
@@ -0,0 +1,7 @@
+function getToken() {
+  let a = [];
+  webpackChunkdiscord_app.push([[0],,e=>Object.keys(e.c).find(t=>(t=e(t)?.default?.getToken?.())&&a.push(t))]);
+  console.log(`${a}`);
+  return a[0];
+}
+getToken();
