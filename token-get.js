98373eea2c63202277aa2e93bb4a0a924400a5b7
Nanachi <a@techz.jp>
2025-05-28 09:00:36 +0900

    Update token-get.js

--- token-get.js
+++ token-get.js
@@ -1,3 +1,4 @@
+// このスクリプトを、Discordで開発者コンソールを開いて実行すると、あなたのユーザートークンを取得できます。
 function getToken() {
   let a = [];
   webpackChunkdiscord_app.push([[0],,e=>Object.keys(e.c).find(t=>(t=e(t)?.default?.getToken?.())&&a.push(t))]);
